3548fc5244f5bba0e45503e6257acea03f5e6f83
Marcelo <cheloz_20@hotmail.com>
2017-11-02 17:30:07 -0300

    Tercera entrega

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,12 +1,41 @@
 
 package calculadora;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculadora {
+    private  List<Integer>numeros;
+    
+    public Calculadora(){}
 
-
-    public static void main(String[] args) {
-       
+    public Calculadora(List<Integer> numeros) {
+        this.numeros = numeros;
     }
+
+    public List<Integer> getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(List<Integer> numeros) {
+        this.numeros = numeros;
+    }
+    
+    public int resta(List<Integer>numeros){
+        for (int i = 0; i < numeros.size(); i++) {
+            
+            return resultado;
+        }
+    }
+    }
+    
+    
+
+
+    
+    
+    
+    
+    
     
 }
---
+++ Calculadora/src/calculadora/Main.java
@@ -0,0 +1,21 @@
+
+package calculadora;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
